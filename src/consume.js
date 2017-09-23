f87fc07009c3e6b3eba1e54470eb4939a4a6c792
Ben Sears <bsears@vampeo.com>
2017-09-23 13:05:05 -0400

    modified consume to work with new pluginbot system

--- src/consume.js
+++ src/consume.js
@@ -4,11 +4,14 @@ import { connect } from 'react-redux';
 export default function(...servicesToConsume){
     return function(Component){
         const mapStateToProps = (state) => {
-
+            if(!state.pluginbot){
+                console.error("pluginbot not initialized, try adding pluginbot provider")
+                return {services : {}};
+            }
             console.log("CONSUMIN!", state);
             let services = servicesToConsume.reduce((acc, service) => {
-                acc[service] = state.services[service];
-                console.log(service, "GOT ME A THING", state.services[service]);
+                acc[service] = state.pluginbot.services[service];
+                console.log(service, "GOT ME A THING", state.pluginbot.services[service]);
                 return acc;
             }, {});
 
